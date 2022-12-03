f0077a51a6873f5d09b5582fa89354945683d2cd
sharangikap <sharangikap@gmail.com>
2022-12-03 12:43:16 +0530

    Add files via upload

---
+++ School_Nonschool.php
@@ -0,0 +1,94 @@
+<html>
+    <head>
+         <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <meta name="description" content="">
+        <meta name="author" content="">
+
+
+        <title>Steam Selecion</title>
+
+        <!-- Bootstrap core CSS -->
+         <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <link href="css/bootstrap.css" rel="stylesheet">
+        <link href="css/bootstrap-theme.css" rel="stylesheet">
+        <link href="css/bootstrap-theme.min.css" rel="stylesheet">
+        <link href="css/font-awesome.css" rel="stylesheet">
+        <link href="css/carousel.css" rel="stylesheet">
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+        
+        <script type="text/javascript" src="js/jquery.min.js"></script>
+        <script type="text/javascript" src="js/bootstrap.js"></script>
+        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+<style>
+p {
+  font-size: 20px;
+}
+
+.btn
+{
+    font-size: 20px;
+}
+
+</style>
+    </head>
+    <body style="background-color:#F8F9F9;">
+
+         <div class="container">
+        <div class="form-group">
+             <hr style="height:3px;border-width:0;color:INDIANRED;background-color:INDIANRED">
+             <hr style="height:3px;border-width:0;color:yellow;background-color:yellow">
+             <hr style="height:3px;border-width:0;color:green;background-color:green">
+              
+           <!-- <a href="index.php" class="btn btn-info btn-lg">
+          <span class="glyphicon glyphicon-home"></span> Home
+        </a> </div>-->
+            
+             <div class="form-group">
+        <h2 class="text-center"> Welcome to the Advanced Level 2023 Admission Registration</h2> <br> <br>
+        <p style= "color:red;"> ලුම්බිණි විදුහල අ.පො.ස. උසස් පෙළ 2023 සදහා මාර්ගගත අයදුමි</p>
+        <p> 1. First Select the relevant stream you wished to apply.</p>
+        <p> 2. Select one of the subject in three categories. Refer the details on subject selection for each category.</p>
+        <p> 3. Make sure to answer for all questions given.</p>
+        <p> 4. You cannot alter your submission so be careful and check thoroughly before submission.</p>
+        <p> 5. You will get a registration reference number after successful submission. Keep that for your future reference</p>
+     <hr><br> 
+         <p style="text-align:center"> <b>Please select one of the stream that you wish to apply.</b></p>
+      <br> <br>
+      <form>
+ <div class="form-group">      
+ <div class="col-sm-1">
+  <button type="button" style = "width:150px" class="btn btn-outline-info btn-xl" value="science" onclick="window.location='Academic_GCE_OL.php';">Science</button>
+</div> 
+ <div class="col-sm-1"> &nbsp; </div>
+<div class="col-sm-1">
+    <button type="button" style = "width:150px" class="btn btn-outline-info btn-xl" value="technology" onclick="window.location='tech_application.php';">Technology</button>
+</div>
+<div class="col-sm-1"> &nbsp; </div>
+  <div class="col-sm-1">
+       <button type="button" style = "width:150px" class="btn btn-outline-info btn-xl" value="commerce" onclick="window.location='com_application.php';">Commerce</button>
+  </div>
+ <div class="col-sm-1"> &nbsp; </div> 
+   <div class="col-sm-1">
+   <button type="button" style = "width:150px" class="btn btn-outline-info btn-xl" value="art" onclick="window.location='art_application.php';">Art</button>
+  </div>
+  <div class="col-sm-1"> &nbsp; </div>  
+      <div class="col-sm-1" >
+          <button type="button" class="btn btn-outline-info btn-xl" value="13_years" onclick="window.location='13years_application.php';">&nbsp;13 Years of Guaranteed Education&nbsp;</button>
+  </div></div>
+  </form>
+
+  </div> 
+
+        
+    </body>
+    
+    </html>
---
+++ index.php
@@ -0,0 +1,5 @@
+<?php
+
+header('Location: http://lumbini.sch.lk/Apply/OL.html');
+
+?>
